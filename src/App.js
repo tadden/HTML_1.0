f0a269077bb2a0988901630d297bb077bf949de9
IljaKazanli <ilja.kazanli@28stone.com>
2025-03-19 22:25:55 +0200

    Added GitHub Pages deployment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="App" style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        title="iframe"
+        src="https://textbook.edu.goit.global/html-css-a7u5xv/v1/docs/lesson-01/intro/"
+        data-tour-step="iframe"
+        style={{ width: "100%", height: "100vh" }}
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
